32d04070de9077b0983ebe9dfe401d7b87405708
Harold Salvador <harold.salvador@tecsup.edu.pe>
2026-03-25 21:34:40 -0500

    agregado StudentView.java

---
+++ src/StudentView.java
@@ -0,0 +1,8 @@
+public class StudentView {
+
+    public void printStudentDetails(String name, String course, int age) {
+        System.out.println("Nombre: " + name);
+        System.out.println("Curso: " + course);
+        System.out.println("Edad: " + age);
+    }
+}
